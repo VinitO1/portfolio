a069eac025ee31e88904a465ddc638623c03b0b8
Vinit Sonawane <106796863+VinitO1@users.noreply.github.com>
2024-07-31 23:52:19 -0700

    update CV and projects

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.render('home');
